ac56e2280e1de4ba8560e1828c0e38a5e8d14cbf
JINHO LEE <wlsgh8309@naver.com>
2023-05-11 18:28:10 +0900

    고객 컴포넌트 구조화하기

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -3,10 +3,33 @@ import React from "react";
 export default function Customer(props) {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>{props.birthday}</p>
-      <p>{props.gender}</p>
-      <p>{props.job}</p>
+      <CustomerProfile image={props.image} name={props.name} id={props.id} />
+      <CustomerInfo
+        birthday={props.birthday}
+        gender={props.gender}
+        job={props.job}
+      />
+    </div>
+  );
+}
+
+function CustomerProfile({ image, name, id }) {
+  return (
+    <div>
+      <img src={image} alt="profile" width="100" />
+      <h2>
+        {name}({id})
+      </h2>
+    </div>
+  );
+}
+
+function CustomerInfo({ birthday, gender, job }) {
+  return (
+    <div>
+      <p>{birthday}</p>
+      <p>{gender}</p>
+      <p>{job}</p>
     </div>
   );
 }
