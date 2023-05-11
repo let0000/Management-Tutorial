30a62a5640ac5e2dcbc15f0ee16e6338f219bd5a
JINHO LEE <wlsgh8309@naver.com>
2023-05-11 18:03:16 +0900

    고객 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Customer from "./components/Customer";
+
+const customer = {
+  name: "홍길동",
+  birthday: "961223",
+  gender: "남자",
+  job: "대학생",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
---
+++ src/components/Customer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Customer(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.birthday}</p>
+      <p>{props.gender}</p>
+      <p>{props.job}</p>
+    </div>
+  );
+}
