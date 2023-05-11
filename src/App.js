3434db3d846b26d736cb8b70c8a47e0ada2f554d
JINHO LEE <wlsgh8309@naver.com>
2023-05-11 18:57:20 +0900

    Material UI 를 이용한 테이블 디자인

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
 import "./App.css";
 import Customer from "./components/Customer";
+import { TableHead, TableBody, TableRow, TableCell } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
-const customer = {
-  name: "홍길동",
-  birthday: "961223",
-  gender: "남자",
-  job: "대학생",
-};
+const Root = styled("div")(({ theme }) => ({
+  width: "100%",
+  marginTop: theme.spacing(3),
+  overflowX: "auto",
+}));
+
+const StyledTable = styled("table")({
+  minWidth: 1080,
+});
+
+const customers = [
+  {
+    id: 1,
+    image: "https://i.ytimg.com/vi/ep2IgZdz0o0/maxresdefault.jpg",
+    name: "팜하니",
+    birthday: "041006",
+    gender: "여자",
+    job: "가수",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "980612",
+    gender: "남자",
+    job: "의적",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/2",
+    name: "김강식",
+    birthday: "020304",
+    gender: "남자",
+    job: "보디빌더",
+  },
+];
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <Root>
+      <StyledTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {customers.map((item) => {
+            return (
+              <Customer
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                birthday={item.birthday}
+                gender={item.gender}
+                job={item.job}
+              />
+            );
+          })}
+        </TableBody>
+      </StyledTable>
+    </Root>
   );
 }
 
--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,35 +1,17 @@
 import React from "react";
+import { TableRow, TableCell } from "@mui/material";
 
 export default function Customer(props) {
   return (
-    <div>
-      <CustomerProfile image={props.image} name={props.name} id={props.id} />
-      <CustomerInfo
-        birthday={props.birthday}
-        gender={props.gender}
-        job={props.job}
-      />
-    </div>
-  );
-}
-
-function CustomerProfile({ image, name, id }) {
-  return (
-    <div>
-      <img src={image} alt="profile" width="100" />
-      <h2>
-        {name}({id})
-      </h2>
-    </div>
-  );
-}
-
-function CustomerInfo({ birthday, gender, job }) {
-  return (
-    <div>
-      <p>{birthday}</p>
-      <p>{gender}</p>
-      <p>{job}</p>
-    </div>
+    <TableRow>
+      <TableCell>{props.id}</TableCell>
+      <TableCell>
+        <img src={props.image} alt="profile" width="64" />
+      </TableCell>
+      <TableCell>{props.name}</TableCell>
+      <TableCell>{props.birthday}</TableCell>
+      <TableCell>{props.gender}</TableCell>
+      <TableCell>{props.job}</TableCell>
+    </TableRow>
   );
 }
