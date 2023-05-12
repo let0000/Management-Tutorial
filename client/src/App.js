542f93b976ac61eb7b7e58aab343e100ef212e76
JINHO LEE <wlsgh8309@naver.com>
2023-05-12 17:19:47 +0900

    고객 REST API 모듈 구현하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
 import { TableHead, TableBody, TableRow, TableCell } from "@mui/material";
@@ -13,34 +14,18 @@ const StyledTable = styled("table")({
   minWidth: 1080,
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://i.ytimg.com/vi/ep2IgZdz0o0/maxresdefault.jpg",
-    name: "팜하니",
-    birthday: "041006",
-    gender: "여자",
-    job: "가수",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/1",
-    name: "홍길동",
-    birthday: "980612",
-    gender: "남자",
-    job: "의적",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/2",
-    name: "김강식",
-    birthday: "020304",
-    gender: "남자",
-    job: "보디빌더",
-  },
-];
-
 function App() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const fetchCustomers = async () => {
+      const response = await fetch("/api/customers");
+      const data = await response.json();
+      setCustomers(data);
+    };
+    fetchCustomers();
+  }, []);
+
   return (
     <Root>
       <StyledTable>
@@ -56,19 +41,21 @@ function App() {
         </TableHead>
 
         <TableBody>
-          {customers.map((item) => {
-            return (
-              <Customer
-                key={item.id}
-                id={item.id}
-                image={item.image}
-                name={item.name}
-                birthday={item.birthday}
-                gender={item.gender}
-                job={item.job}
-              />
-            );
-          })}
+          {customers
+            ? customers.map((item) => {
+                return (
+                  <Customer
+                    key={item.id}
+                    id={item.id}
+                    image={item.image}
+                    name={item.name}
+                    birthday={item.birthday}
+                    gender={item.gender}
+                    job={item.job}
+                  />
+                );
+              })
+            : ""}
         </TableBody>
       </StyledTable>
     </Root>
--- server.js
+++ server.js
@@ -2,12 +2,39 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://i.ytimg.com/vi/ep2IgZdz0o0/maxresdefault.jpg",
+      name: "팜하니",
+      birthday: "041006",
+      gender: "여자",
+      job: "가수",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/1",
+      name: "홍길동",
+      birthday: "980612",
+      gender: "남자",
+      job: "의적",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/2",
+      name: "김강식",
+      birthday: "020304",
+      gender: "남자",
+      job: "보디빌더",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
